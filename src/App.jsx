b3bd30d61026df5156ee47fd971ade2feb33628c
Henry Ripple <hripple46@gmail.com>
2023-08-05 22:20:47 -0500

    changed 'up' button to svg

--- src/App.jsx
+++ src/App.jsx
@@ -382,17 +382,32 @@ function App() {
         </div>
         <Projects projectsRef={projectsRef} />
         <Contact contactRef={contactRef} />
-        <p
-          className="fixed bottom-0 right-0 mr-4 mb-4 text-white text-sm md:text-2xl cursor-pointer"
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="48"
+          height="48"
+          viewBox="0 -960 960 960"
+          fill="white"
+          className="fixed bottom-0 right-0 mr-4 mb-4 rotate-180 
+           cursor-pointer"
           onClick={() => {
             window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
           }}
         >
-          Back To Top
-        </p>
+          <path d="M480-351l173-173-43-42-130 130-130-130-43 42 173 173zm0 271q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140zm0-340z"></path>
+        </svg>
       </div>
     </>
   );
 }
 
 export default App;
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="48"
+  height="48"
+  viewBox="0 -960 960 960"
+>
+  <path d="M480-351l173-173-43-42-130 130-130-130-43 42 173 173zm0 271q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140zm0-340z"></path>
+</svg>;
